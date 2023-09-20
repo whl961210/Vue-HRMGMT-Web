764389bfea86af202edc81bfd549dd0a1eeb1151
HanlunWang <whl961210@gmail.com>
2023-09-19 22:37:49 -0700

    added router

---
+++ src/router/index.vue
@@ -0,0 +1,24 @@
+import vue from 'vue'
+import vueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+vue.use(vueRouter)
+
+    const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about/',
+        name: 'About',
+        component: () => import('../views/About.vue')
+    },
+]
+
+const router = new vueRouter({
+    routes
+})
+export default router
+
